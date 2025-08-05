e0482d9ebb669d696c9a644065121507986d7942
ragpark <pcoyne@bcs.org>
2025-08-05 14:34:42 +0100

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@localhost:5432/training")
+DATABASE_URL = os.getenv("DATABASE_URL", "${{ Postgres.DATABASE_URL }}")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
